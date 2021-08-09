509cb497a2f58aedf7cd69e6f398de9bdfaf49d9
Lex van der Werff <dev@lexvanderwerff.com>
2021-08-09 09:01:10 +0200

    Removed unused stuff

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,5 +1,4 @@
 import { Disclosure } from '@headlessui/react';
-import { SearchIcon } from '@heroicons/react/solid';
 import Form from '../form/Form';
 import Card from '../ui/Card';
 import Todos from '../todo/Todos';
@@ -42,23 +41,6 @@ const Layout = (props) => {
 				<Card padding={false}>
 					<Todos todos={todos} />
 				</Card>
-				{/* <Card>
-					<label htmlFor="search" className="sr-only">
-						Search
-					</label>
-					<div className="relative text-gray-400 focus-within:text-gray-600">
-						<div className="pointer-events-none absolute inset-y-0 left-0 pl-3 flex items-center">
-							<SearchIcon className="h-5 w-5" aria-hidden="true" />
-						</div>
-						<input
-							id="search"
-							className="block w-full bg-white py-2 pl-10 pr-3 border border-transparent rounded-md leading-5 text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-indigo-600 focus:ring-white focus:border-white sm:text-sm"
-							placeholder="Search"
-							type="search"
-							name="search"
-						/>
-					</div>
-				</Card> */}
 			</main>
 		</div>
 	)
--- src/components/ui/Empty.js
+++ src/components/ui/Empty.js
@@ -1,14 +1,4 @@
-/* This example requires Tailwind CSS v2.0+ */
-import { PlusIcon } from '@heroicons/react/solid'
-
 export default function Empty() {
-	const newTodoInput = document.getElementById("todo");
-
-	const onNewTodoHandler = () => {
-		console.log(newTodoInput);
-		// newTodoInput.focus();
-	};
-
 	return (
 		<div className="text-center">
 			<svg className="mx-auto h-12 w-12 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
@@ -16,16 +6,6 @@ export default function Empty() {
 			</svg>
 			<h3 className="mt-2 text-sm font-medium text-gray-900">No todos</h3>
 			<p className="mt-1 text-sm text-gray-500">Get started by creating a new todo.</p>
-			<div className="mt-6">
-				<button
-					type="button"
-					onClick={onNewTodoHandler}
-					className="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-				>
-					<PlusIcon className="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
-					New Todo
-				</button>
-			</div>
 		</div>
 	)
 }
