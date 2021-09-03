c68a2c53ee08d83d09293ca0fd3933e13aa0cc90
Lex van der Werff <dev@lexvanderwerff.com>
2021-09-03 21:33:06 +0200

    Added localstorage

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,23 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { notificationsReducer } from "./notifications";
 import { todosReducer } from "./todos";
+import { save, load } from "redux-localstorage-simple";
 
 const store = configureStore({
+	middleware: [save({
+		states: ["todos"],
+		namespace: "app",
+		namespaceSeparator: "-"
+	})],
 	reducer: {
 		todos: todosReducer,
 		notifications: notificationsReducer
-	}
+	},
+	preloadedState: load({
+		states: ["todos"],
+		namespace: "app",
+		namespaceSeparator: "-"
+	})
 });
 
 export default store;
