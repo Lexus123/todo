63de5a456519fac59643414a595c40d7c4c205b5
Lex van der Werff <dev@lexvanderwerff.com>
2021-09-05 11:59:30 +0200

    Uupdated header

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,25 +1,12 @@
 import { ChevronDoubleLeftIcon } from '@heroicons/react/outline';
-import { Disclosure, Switch } from '@headlessui/react';
-import { useDispatch, useSelector } from 'react-redux';
-import { themesActions } from '../../store/themes';
+import { Disclosure } from '@headlessui/react';
 import { Link, useLocation } from 'react-router-dom';
-
-function classNames(...classes) {
-	return classes.filter(Boolean).join(' ')
-}
+import { CogIcon } from '@heroicons/react/solid';
 
 const Header = (props) => {
-	const dispatch = useDispatch();
-
-	const theme = useSelector(state => state.themes.theme);
-
 	const location = useLocation();
 
 	const isTodosPage = location.pathname === "/todos";
-
-	const changeThemeHandler = () => {
-		dispatch(themesActions.toggleTheme());
-	};
 
 	return (
 		<div className="bg-gradient-to-r from-indigo-500 via-indigo-500 to-indigo-600 dark:from-indigo-400 dark:via-indigo-400 dark:to-indigo-500 pb-32">
@@ -34,27 +21,12 @@ const Header = (props) => {
 									<h1 className="text-3xl font-bold text-white">{props.title}</h1>
 								</div>
 							</Link>
-							<Switch.Group as="div" className="flex items-center">
-								<Switch
-									checked={theme === "dark"}
-									onChange={changeThemeHandler}
-									className={classNames(
-										theme === "dark" ? 'bg-indigo-600' : 'bg-gray-200',
-										'relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
-									)}
-								>
-									<span
-										aria-hidden="true"
-										className={classNames(
-											theme === "dark" ? 'translate-x-5' : 'translate-x-0',
-											'pointer-events-none inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200'
-										)}
-									/>
-								</Switch>
-								<Switch.Label as="span" className="ml-3">
-									<span className="text-sm font-medium text-white">Dark mode</span>
-								</Switch.Label>
-							</Switch.Group>
+							<Link
+								to="/settings"
+								className="inline-flex items-center p-1 text-white focus:outline-none"
+							>
+								<CogIcon className="h-6 w-6 mr-1" aria-hidden="true" /> <span>Settings</span>
+							</Link>
 						</div>
 					</div>
 				</div>
--- src/pages/SettingsPage.js
+++ src/pages/SettingsPage.js
@@ -35,7 +35,7 @@ const SettingsPage = () => {
 						checked={theme === "dark"}
 						onChange={changeThemeHandler}
 						className={classNames(
-							theme === "dark" ? 'bg-indigo-600' : 'bg-gray-300',
+							theme === "dark" ? 'bg-indigo-500' : 'bg-gray-300',
 							'relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
 						)}
 					>
