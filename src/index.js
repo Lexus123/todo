81f7842c10aa1d8b8db71fa053c2f6b048a32baf
Lex van der Werff <dev@lexvanderwerff.com>
2021-09-07 21:24:35 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App'; import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 
